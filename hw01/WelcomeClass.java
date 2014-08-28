5b2db4d351aeedfdd2f242ffb830e097d6b5d9c8
mgavin252 <msg216@lehigh.edu>
2014-08-28 12:54:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print statement with network ID
+        System.out.println("    -----------     ");
+        System.out.println("    | WELCOME |     ");
+        System.out.println("    -----------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--S--G--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This summer I worked at an internship and learned a lot about AutoCad");
+    }
+}
