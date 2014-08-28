dbecadbcac5f129c5de44eecb2b80e6f4c47cea1
mgavin252 <msg216@lehigh.edu>
2014-08-28 00:42:08 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//define a class
+public class HelloWorld{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement with box around it
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("#                    #");
+        System.out.println("# HELLO WORLD 2014!  #");
+        System.out.println("#                    #");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("######################");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}
